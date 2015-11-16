d3a6662805dff377720abb264905e9792aa45534
knolegsb <seon2you@hotmail.com>
2015-11-16 11:17:47 -0800

    lowest level katana component

--- TryKatana/Program.cs
+++ TryKatana/Program.cs
@@ -2,17 +2,19 @@
 using Owin;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TryKatana
 {
+    using AppFunc = Func<IDictionary<string, object>, Task>;
     class Program
     {
         static void Main(string[] args)
         {
-            string uri = "http://localhost:8000";
+            string uri = "http://localhost:8080";
 
             using (WebApp.Start<Startup>(uri))
             {
@@ -27,10 +29,31 @@ namespace TryKatana
     {
         public void Configuration(IAppBuilder app)
         {
-            app.Run(ctx =>
+            app.Use<HelloWorldComponent>();
+
+            //app.UseWelcomePage();
+            //app.Run(ctx =>
+            //{
+            //    return ctx.Response.WriteAsync("Hello World");
+            //});
+        }
+    }
+
+    public class HelloWorldComponent
+    {
+        AppFunc _next;
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            //await _next(environment);
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response))
             {
-                return ctx.Response.WriteAsync("Hello World");
-            });
+                return writer.WriteAsync("Hello!");
+            }
         }
     }
 }
